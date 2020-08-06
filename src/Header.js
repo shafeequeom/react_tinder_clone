d9c2931d8eaf44e197bde622db89b525179ca0f2
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2020-08-06 16:14:57 +0400

    Install Matgerail UI

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+
+function Header() {
+  return (
+    <div>
+      <PersonIcon />
+    </div>
+  );
+}
+
+export default Header;
