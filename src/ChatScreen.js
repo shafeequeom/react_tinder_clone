9ce30e42c23cda6b348fd508ef848124506ea8e7
Shafeeque OM <5572374-shafeequekrews@users.noreply.gitlab.com>
2020-08-07 01:03:51 +0400

    Message Chat Screen

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -4,6 +4,7 @@ import "./ChatScreen.css";
 import { Avatar } from "@material-ui/core";
 
 function ChatScreen() {
+  const [input, setInput] = useState();
   const [messages, setMessages] = useState([
     {
       name: "Ellen",
@@ -21,21 +22,47 @@ function ChatScreen() {
       message: "I am great!",
     },
   ]);
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
   return (
     <div className="chatScreen">
       <p className="chatScreen__timestamp">
         YOU MATCHED WITH ELLEN ON 10TH JUNE 2020
       </p>
-      {messages.map((message) => (
-        <div className="chatScreen__message">
-          <Avatar
-            className="chatScreen__image"
-            alt={message.name}
-            src={message.image}
-          />
-          <p className="chatScreen__text">{message.message}</p>
-        </div>
-      ))}
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chatScreen__message">
+            <p className="chatScreen__textUser">{message.message}</p>
+          </div>
+        )
+      )}
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          placeholder="Type your message"
+        />
+        <button
+          onClick={handleSend}
+          type="submit"
+          className="chatScreen__inputButton"
+        >
+          SEND
+        </button>
+      </form>
     </div>
   );
 }
